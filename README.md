ccc8bfd6a6413a0a1b4e78a25f3248f4cd50be7a
RamboxEu <mail.rambox@gmail.com>
2019-02-18 19:25:23 +0100

    Initial commit
    Project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Needle API
+API that helps you building mods on [Fabric API](https://fabricmc.net)
---
+++ src/main/kotlin/eu/rambox/needleapi/NeedleAPI.kt
@@ -0,0 +1,7 @@
+package eu.rambox.needleapi
+import net.fabricmc.api.ModInitializer
+import org.apache.logging.log4j.LogManager
+
+object NeedleAPI : ModInitializer {
+    override fun onInitialize() {}
+}
